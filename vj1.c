b11e30d28567adbd781da72b817f19cfbedc824f
slanacpizzagomilicafesb <56551410+slanacpizzagomilicafesb@users.noreply.github.com>
2019-11-20 21:09:30 +0100

    Update vj1.c

--- vj1.c
+++ vj1.c
@@ -18,28 +18,31 @@ struct _student {
 
 int BrojanjeRedaka(char*);
 int UcitavanjeIzDatoteke(pstud, char*);
-int Ispis(pstud, char*);
+int Ispis(pstud, char*, int);
 
 int main()
 {
 	pstud st = NULL;
 	char *ime_datoteke_za_unos = NULL;
+	int brojRedaka = 0;
 
 	ime_datoteke_za_unos = (char*)malloc(MAX_STRLEN * sizeof(char));
 	if (ime_datoteke_za_unos == NULL) return -1;
 
+	brojRedaka = BrojanjeRedaka(ime_datoteke_za_unos);
+
 	printf("Unesite ime datoteke za unos: ");
 	scanf("%s", ime_datoteke_za_unos);
-	if (strrchr(ime_datoteke_za_unos, '.') == NULL) 
+	if (strrchr(ime_datoteke_za_unos, '.') == NULL)
 		strcat(ime_datoteke_za_unos, ".txt");
 
-	st = (pstud)malloc(BrojanjeRedaka(ime_datoteke_za_unos) * sizeof(_STUDENT));
-	if (st = NULL) return - 1;
+	st = (pstud)malloc(brojRedaka * sizeof(_STUDENT));
+	if (st = NULL) return -1;
 
-	printf("Retci: %d\n", BrojanjeRedaka(ime_datoteke_za_unos));
+	printf("Retci: %d\n", brojRedaka);
 
 	UcitavanjeIzDatoteke(st, ime_datoteke_za_unos);
-	Ispis(st, ime_datoteke_za_unos);
+	Ispis(st, ime_datoteke_za_unos, brojRedaka);
 
 	system("pause");
 	return 0;
@@ -63,26 +66,26 @@ int BrojanjeRedaka(char* ime_datoteke)
 
 int UcitavanjeIzDatoteke(pstud st, char* ime_datoteke)
 {
-	int i = 0;
 	FILE* dat = NULL;
 	dat = fopen(ime_datoteke, "r");
 	if (dat == NULL) return -1;
 	while (!feof(dat))
 	{
-		fscanf(dat, "%s %s %d", st[i].ime, st[i].prezime, &st[i].bodovi);
-		st[i].rel_bod = (double)st[i].bodovi / MAX_BOD * 100;
-		i++;
+		fscanf(dat, "%s %s %d", st->ime, st->prezime, &st->bodovi);
+		st->rel_bod = (double)st->bodovi / MAX_BOD * 100;
+		st++;
 	}
 	fclose(dat);
 	return 0;
 }
 
-int Ispis(pstud st, char* ime_datoteke)
+int Ispis(pstud st, char* ime_datoteke, int brojRedova)
 {
 	int i = 0;
-	for (i = 0; i <= BrojanjeRedaka(ime_datoteke); i++)
+	for (i = 0; i <= brojRedova; i++)
 	{
-		printf("%s %s %d %lf", st[i].ime, st[i].prezime, st[i].bodovi, st[i].rel_bod);
+		printf("%s %s %d %lf", st->ime, st->prezime, st->bodovi, st->rel_bod);
+		st++;
 	}
 	return 0;
 }
